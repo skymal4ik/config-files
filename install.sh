4ab1031a9a95a5bbdc7796eaad834699984ae766
Dmitri Medvedev <root@dev.webcentrale.eu>
2016-07-01 08:51:36 -0400

    Added initial install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.nanorc
+wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.tmux.conf
+wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.vimrc
