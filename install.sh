88c38fd262b1c544f8d94458323a079a763065f4
Dmitri <dimka@stv.ee>
2016-12-16 14:00:39 +0200

    Update install.sh - ca-certificates note
    Update install.sh - Added note & command to install ca-certificates package

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If wget gives 'Certificate not trusted' error, you might needt to install 'ca-certificates' package
+# apt-get install ca-certificates;
+
+apt-get install ca-certificates;
+
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.nanorc -O ~/.nanorc
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.tmux.conf -O ~/.tmux.conf
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.vimrc -O ~/.vimrc
