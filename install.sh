2d17cf9aa7ed22f6a245930a33edcc4c5ecf8506
Dmitri <dimka@stv.ee>
2017-01-27 16:16:58 +0200

    Update install.sh - added installation of tmux and vim

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# If wget gives 'Certificate not trusted' error, you might needt to install 'ca-certificates' package
-# apt-get install ca-certificates;
+# Install required packages:
+apt-get install vim tmux;
 
+# If wget gives 'Certificate not trusted' error, you might needt to install 'ca-certificates' package
 apt-get install ca-certificates;
 
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.nanorc -O ~/.nanorc
