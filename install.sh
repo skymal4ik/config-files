f50889be376f33aa4ac204915e174bf2bc612c7f
Dmitri <dimka@stv.ee>
2017-07-30 00:35:05 +0300

    Updated install.sh -added apt force flag
    apt won't ask for installation confirmation

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install required packages:
-apt-get install vim tmux;
+apt-get install vim tmux -y;
 
 # If wget gives 'Certificate not trusted' error, you might needt to install 'ca-certificates' package
-apt-get install ca-certificates;
+apt-get install ca-certificates -y;
 
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.nanorc -O ~/.nanorc
 wget --quiet https://raw.githubusercontent.com/skymal4ik/config-files/master/.vimrc -O ~/.vimrc
